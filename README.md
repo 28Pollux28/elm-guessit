1a4b995104c5cac545173e45ef0cca8c5641e0b8
28Pollux28 <lemaire_valentin28@yahoo.fr>
2023-02-03 22:22:30 +0100

    Added CSS and Readme

---
+++ README.md
@@ -0,0 +1,36 @@
+[![Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License](https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png)]("http://creativecommons.org/licenses/by-nc-sa/4.0/" "Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License")  
+This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+
+---
+
+# Projet ELM Valentin LEMAIRE - Gaspard O'MAHONY
+
+---
+
+## Description
+
+Implémentation du Jeu Guess It ! A partir des définitions affichées, trouvez le mot caché !
+
+## Utilisation
+
+Lancer la commande à la racine du dossier
+```bash
+elm reactor
+```
+Puis ouvrir le fichier index.html
+
+### Remarques
+Ne pas recompiler le fichier HTML. Il contient des entrées de scripts Javascript et de feuilles de styles nécessaires pour le fonctionnement de Bootstrap
+
+---
+## Auteurs
+
+* **Valentin LEMAIRE** - *Initial work* - [Valentin Lemaire](https://github.com/28Pollux28)
+* **Gaspard O'MAHONY** - *Initial work* - [gaspardomahony](https://github.com/gaspardomahony)
+
+---
+
+## License
+
+This project is licensed under the terms of the CC BY-NC-SA 4.0 license.
+
--- src/Main2.elm
+++ src/Main2.elm
@@ -2,8 +2,8 @@ module Main2 exposing (..)
 
 
 import Browser
-import Html exposing (Html, button, div, h1, input, li, ol, p, text, ul)
-import Html.Attributes exposing (style)
+import Html exposing (Html, br, button, div, h1, input, li, ol, p, span, text, ul)
+import Html.Attributes as Html exposing (attribute, class, tabindex, value)
 import Html.Events exposing (onClick, onInput)
 import Http
 import Json.Decode exposing (Decoder)
@@ -12,7 +12,7 @@ import Task
 
 main = Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
 
-type Etat = Failure Http.Error | Loading | Success
+type Etat = Failure | Loading | Success
 
 type alias Model =
   {
@@ -24,14 +24,18 @@ type alias Model =
   , data : List Datas
   , show : Bool
   , found : Bool
+  , content : String
+  , shouldCheck : Bool
+  , count : Int
   }
 
 type Msg = GotListM (Result Http.Error String)
  | Mot String
  | RandInt Int
  | JSonDef (Result Http.Error (List Datas))
- | Show
  | Change String
+ | Retry
+ | Try
 
 type alias Datas =
   {
@@ -52,7 +56,7 @@ type alias Definition =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    (Model Loading Loading [] "" 0 [] False False, getList )
+    (Model Loading Loading [] "" 0 [] False False "" False 0, getList )
 
 
 
@@ -60,26 +64,34 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GotListM (Err err) ->
-            ({model | httpState = Failure err}, Cmd.none )
+            ({model | httpState = Failure}, getList2 )
         GotListM (Ok string) ->
             ({model | listeM = String.words string, httpState = Success}, getRandInt (List.length (String.words string)))
         Mot string ->
-            ({model |mot = string}, getJson string)
+            ({model | mot = string}, getJson string)
         RandInt int ->
             case getElemList model.listeM int of
                 Nothing -> (model, Cmd.none)
                 Just mot -> ({model | number = int}, getMot mot)
         JSonDef (Err err) ->
-            ({model | jsonState = Failure err}, Cmd.none)
+            ({model | jsonState = Failure}, Cmd.none)
         JSonDef (Ok datas) ->
             ({model | jsonState = Success, data = datas}, Cmd.none)
-        Show ->
-            ({model| show = not model.show}, Cmd.none)
         Change str->
-            if str == model.mot then
-              ({model | found = True},Cmd.none)
+            ({model | content = str, shouldCheck = False},Cmd.none)
+        Try ->
+            if model.content == model.mot && not model.found then
+              ({model | found = True, count = model.count + 1 },Cmd.none)
+            else if not (model.content == "") then
+              ({model | shouldCheck = True},Cmd.none)
             else
-              (model,Cmd.none)
+              (model, Cmd.none)
+        Retry ->
+            if model.found then
+              ({model | content="", show = False, found = False, shouldCheck = False, jsonState = Loading},getRandInt (List.length model.listeM))
+            else
+              ({model | content="", show = False, found = False, shouldCheck = False, jsonState = Loading, count = 0},getRandInt (List.length model.listeM))
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -87,45 +99,87 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    div[style "text-align" "left", style "margin-left" "5em"][
-      if not model.show then
+    div[class "p-5"][
+      if ((model.httpState == Success) && (model.jsonState == Success)) then
         div[][
-          h1[][text "Guess It !"],
-          div[] (textDatas model.data)
+          div[][
+            span[class "d-flex justify-content-center"][h1[Html.class "p-2  border-bottom border-3 border-primary"][text "Guess It !"]],
+            ul[class "list-group"][li[class "list-group-item list-group-item-primary"][text "Définition : "], ul[class "list-group-item list-group-item-secondary"](textDatas model.data)]
+          ],
+          div[class "modal", tabindex -1, Html.id "modalAnswer", Html.attribute "aria-hidden" "true", attribute "data-bs-backdrop" "static", attribute "data-bs-keyboard" "false"][
+            div[class "modal-dialog modal-dialog-centered"][
+              div[class "modal-content"][
+                div[class "modal-header"][
+                  h1[class "modal-title"][text "The answer was ",span[class "text-danger"][text model.mot]],
+                  button[class "btn-close", attribute "data-bs-dismiss" "modal", onClick Retry][]
+                ]
+              ]
+            ]
+          ],
+          div[class "d-flex justify-content-center"][p[class ""][text "Try your luck !"]],
+          div[class "d-flex justify-content-center"][p[][text "So far, you've found ",span[class "badge rounded-pill text-bg-secondary"][text (String.fromInt model.count)], text " words consecutively"]],
+          div[class "d-flex justify-content-center mb-3"][
+          div[class "w-50 input-group"][
+          input[class "form-control", onInput Change, value model.content][],
+          button[class "btn btn-outline-primary", onClick Try][text "Guess !"]
+          ]],
+          if model.found then
+            div[class "d-flex justify-content-center"][p[class "alert alert-success"][text ("You found the word ! It was " ++ model.mot ++ " !")]]
+          else
+            if model.shouldCheck then
+              div[class "d-flex justify-content-center"][p[class "alert alert-danger"][text "Nope, try again !"]]
+            else
+              div[][],
+          br[][],
+          div[class "d-flex justify-content-center my-1"][
+            button[class "btn btn-warning", onClick Retry][text "Try Again !"]
+          ],
+          div[class "d-flex justify-content-center my-1"][
+            button[class "btn btn-danger", Html.attribute "data-bs-toggle" "modal", Html.attribute "data-bs-target" "#modalAnswer"][text "Show me the Answer !"]
+          ]
         ]
-      else
-        div[][
-          h1[][text model.mot],
-          div[] (textDatas model.data)
-        ],
-      p[][text "Try your luck !"],
-      input[onInput Change][],
-      if model.found then
-        div[][p[][text ("You found the word ! It was " ++ model.mot ++ " !")]]
-      else
-        div[][text "Nope, try again !"],
-      button[onClick Show][text "Show me the Answer !"]
+    else
+      div[][
+        span[class "d-flex justify-content-center"][h1[Html.class "p-2  border-bottom border-3 border-primary"][text "Guess It !"]],
+        if model.jsonState == Failure then
+          div[][
+            span[class "d-flex justify-content-center my-5"][span[class "text-danger"][text "Something went wrong while fetching the definitions. Maybe the API is down ?"]],
+            span[class "d-flex justify-content-center"][button[class "btn btn-danger", onClick Retry][text "Retry"]]
+          ]
+        else if model.httpState == Failure then
+          div[][
+            span[class "d-flex justify-content-center my-5"][span[class "text-danger"][text "Something went wrong while fetching the words"]]
+          ]
+        else
+          div[][
+            span[class "d-flex justify-content-center my-5"][span[class "spinner-border text-primary"][]],
+            span[class "d-flex justify-content-center"][span[][text "Loading..."]]
+          ]
+      ]
     ]
 
-textDatas: List Datas -> List (Html Msg)
+textDatas: List Datas -> List(Html Msg)
 textDatas lData=
     case lData of
         []->[]
-        (x::xs)-> [ul[]([text "Définition : "] ++ [li[](textMeanings x.meanings)])] ++ (textDatas xs)
-textMeanings: List Meaning -> List (Html Msg)
+        (x::xs)-> [ul[class "list-group"](textMeanings x.meanings)] ++ textDatas xs
+textMeanings: List Meaning -> List(Html Msg)
 textMeanings lMeanings =
     case lMeanings of
-        []->[]
-        (x::xs)-> [li[]([text x.partOfSpeech] ++ [ol[] (textDefinitions x.definitions)])] ++ textMeanings xs
+        []-> []
+        (x::xs)-> [li[class "list-group-item list-group-item-dark"][text x.partOfSpeech,ol[class "list-group list-group-flush list-group-numbered"](textDefinitions x.definitions)]] ++ textMeanings xs
 
 textDefinitions: List Definition -> List (Html Msg)
 textDefinitions lDefinitions =
     case lDefinitions of
         []->[]
-        (x::xs)-> [li[]([text x.definition])] ++ textDefinitions xs
+        (x::xs)-> [li[class "list-group-item list-group-item-light"][text x.definition]] ++ textDefinitions xs
 getList : Cmd Msg
 getList =
-    Http.get { url = "http://localhost:8000/src/thousand_words_things_explainer.txt", expect = Http.expectString GotListM }
+    Http.get { url = "src/thousand_words_things_explainer.txt", expect = Http.expectString GotListM }
+getList2 : Cmd Msg
+getList2 =
+    Http.get { url = "thousand_words_things_explainer.txt", expect = Http.expectString GotListM }
 
 getElemList : List a -> Int -> Maybe a
 getElemList list index =
